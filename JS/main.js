58e5ac6507ff1a8d0bbb75dce144d498ccd15dd7
Artem Kril <arturkril9@gmail.com>
2026-03-20 00:36:56 +0200

    HM JS

---
+++ JS/main.js
@@ -0,0 +1,65 @@
+// HM1
+
+//***1***
+// let UserName = 'Ivan';
+// let city = UserName;
+// console.log(city);
+
+//***2***
+//Який буде результат виконання скрипта?
+// let name = "Olga";
+// console.log(`привіт ${1}`);  // тут ми просто в шлаблонних літералах вказуємо число і воно його показує, оскільки не має доступу до змінної чи ключа.
+// console.log(`привіт ${"name"}`); // тут просто ми вказали всередині лапок рядок, який і по суті консоль нам показує.
+// console.log(`привіт ${name}`); // тут напряму ми вказали змінну і правильно це написали. 
+
+// ***3***
+// let a = "5";
+// let b = "13cvb";
+// let c = "12.9sxdcfgv";
+// console.log(Number(a), parseInt(b), parseFloat(c));
+// console.log(typeof(Number(a)), typeof(parseInt(b)), typeof(parseFloat(c)));
+// console.log(typeof(a), typeof(b), typeof(c));
+
+//***4***
+// console.log(((0.1 * 10) + (0.2 * 10)) / 10);
+
+//***5**
+// const numArr = [20, 10, 50, 40];
+// const max = Math.max(...numArr)
+// console.log(max);
+
+//***6***
+// let min = 2;
+// let max = 4;
+// let random = Math.floor(Math.random() * (max- min + 1))+min;
+// console.log(random);
+
+//***7***
+// const message = "Welcome to Bahamas!";
+// console.log(message.length);
+
+//***8***
+// const message = "Welcome to Bahamas!";
+// console.log(message.toUpperCase());
+
+// ***9***
+// let  player = {
+// };
+// player.name = 'Artem';
+// player.age = 25;
+// player.city = 'Lviv';
+// console.log(player);
+
+// delete player.city;
+// player ["like flowers"] = false;
+// console.log(player);
+
+// for (let key in player) {
+//     console.log(player [key]);
+// }
+
+
+
+
+
+
